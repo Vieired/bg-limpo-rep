e41f1cfdd0e4d4ef8f3ddfb1336227b9f6627544
Ednaldo Gomes <egomes@metatron.com.br>
2026-01-13 22:43:35 -0300

    feat(global): adiciona novo componente de spinner grande

---
+++ src/components/BigSpinner/index.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import loader from '@assets/loader3.gif';
+
+const Spinner = styled.div`
+    width: 100%;
+    min-height: 250px;
+    background: url(${loader}) center no-repeat;
+    background-size: 5rem;
+`;
+
+export const BigSpinner: React.FC = () => <Spinner/>
