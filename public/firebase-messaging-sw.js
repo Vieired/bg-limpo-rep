bf831cec95a78e022a253bdad5764570150f6cc5
Ednaldo Gomes <egomes@metatron.com.br>
2026-01-17 01:43:10 -0300

    chore(push): testa solução para parar de duplicar notificações

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+// importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
+// importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
 
 // Sua config normal do Firebase
 firebase.initializeApp({
@@ -10,28 +10,9 @@ firebase.initializeApp({
   messagingSenderId: "245775062234",
   appId: "1:245775062234:web:5ad4705676304ef9e301e3",
 });
-
+/*
 // Inicializa o messaging
 const messaging = firebase.messaging();
-
-/*
-// NÃO CHAME self.registration.showNotification
-// Deixe o navegador exibir automaticamente.
-// Apenas logue o payload.
-messaging.onBackgroundMessage((payload) => {
-  console.log("[firebase-messaging-sw.js] Background message:", payload);
-});
-*/
-
-/*
-messaging.onBackgroundMessage((payload) => {
-  console.log("[firebase-messaging-sw.js] Background message:", payload);
-
-  self.registration.showNotification(payload.data.title, {
-    body: payload.data.body,
-  });
-});
-*/
 
 // Notificação quando o app está FECHADO
 messaging.onBackgroundMessage((payload) => {
@@ -44,3 +25,4 @@ messaging.onBackgroundMessage((payload) => {
     icon
   });
 });
+*/
