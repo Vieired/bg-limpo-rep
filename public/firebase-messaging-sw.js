5ffae9ad309b46422a4b2139680033bc15076d8a
Ednaldo Gomes <egomes@metatron.com.br>
2026-01-17 02:53:00 -0300

    chore(push): testa solução para parar de duplicar notificações

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,24 +1,7 @@
-self.addEventListener("push", (event) => {
-  if (!event.data) return;
-
-  const data = event.data.json();
-
-  const title = data.data?.title ?? "Notificação";
-  const options = {
-    body: data.data?.body,
-    image: data.data?.image || undefined,
-    icon: "/icon-192.png",
-  };
-
-  event.waitUntil(
-    self.registration.showNotification(title, options)
-  );
-});
-
 // importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
 // importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
 
-/*// Sua config normal do Firebase
+// Sua config normal do Firebase
 firebase.initializeApp({
   apiKey: "AIzaSyCRsQbOu4JUqBODv2MRG0PXz0DTKDRZcXg",
   authDomain: "bg-limpo.firebaseapp.com",
@@ -26,20 +9,36 @@ firebase.initializeApp({
   storageBucket: "bg-limpo.firebasestorage.app",
   messagingSenderId: "245775062234",
   appId: "1:245775062234:web:5ad4705676304ef9e301e3",
-});*/
-/*
-// Inicializa o messaging
+});
+
+// // Inicializa o messaging
+// const messaging = firebase.messaging();
+
+// // Notificação quando o app está FECHADO
+// messaging.onBackgroundMessage((payload) => {
+//   console.log("[firebase-messaging-sw.js] Background message:", payload);
+
+//   const { title, body, icon } = payload.notification;
+
+//   self.registration.showNotification(title, {
+//     body,
+//     icon
+//   });
+// });
+
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+
 const messaging = firebase.messaging();
 
-// Notificação quando o app está FECHADO
 messaging.onBackgroundMessage((payload) => {
-  console.log("[firebase-messaging-sw.js] Background message:", payload);
+  console.log("📩 Push em background recebido:", payload);
 
-  const { title, body, icon } = payload.notification;
+  const { title, body, image } = payload.data || {};
 
-  self.registration.showNotification(title, {
+  self.registration.showNotification(title || "Notificação", {
     body,
-    icon
+    image,
+    icon: "/icon-192.png",
   });
 });
-*/
