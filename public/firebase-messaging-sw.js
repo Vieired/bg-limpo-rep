3bf5f5d83dc4d09ea23059d2be403feb08801e4b
Ednaldo Gomes <egomes@metatron.com.br>
2026-01-17 02:58:33 -0300

    chore(push): corrige solução para parar de duplicar notificações

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,15 +1,15 @@
-// importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
-// importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+// // importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
+// // importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
 
-// Sua config normal do Firebase
-firebase.initializeApp({
-  apiKey: "AIzaSyCRsQbOu4JUqBODv2MRG0PXz0DTKDRZcXg",
-  authDomain: "bg-limpo.firebaseapp.com",
-  projectId: "bg-limpo",
-  storageBucket: "bg-limpo.firebasestorage.app",
-  messagingSenderId: "245775062234",
-  appId: "1:245775062234:web:5ad4705676304ef9e301e3",
-});
+// // Sua config normal do Firebase
+// firebase.initializeApp({
+//   apiKey: "AIzaSyCRsQbOu4JUqBODv2MRG0PXz0DTKDRZcXg",
+//   authDomain: "bg-limpo.firebaseapp.com",
+//   projectId: "bg-limpo",
+//   storageBucket: "bg-limpo.firebasestorage.app",
+//   messagingSenderId: "245775062234",
+//   appId: "1:245775062234:web:5ad4705676304ef9e301e3",
+// });
 
 // // Inicializa o messaging
 // const messaging = firebase.messaging();
@@ -28,6 +28,15 @@ firebase.initializeApp({
 
 importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCRsQbOu4JUqBODv2MRG0PXz0DTKDRZcXg",
+  authDomain: "bg-limpo.firebaseapp.com",
+  projectId: "bg-limpo",
+  storageBucket: "bg-limpo.firebasestorage.app",
+  messagingSenderId: "245775062234",
+  appId: "1:245775062234:web:5ad4705676304ef9e301e3",
+});
 
 const messaging = firebase.messaging();
 
