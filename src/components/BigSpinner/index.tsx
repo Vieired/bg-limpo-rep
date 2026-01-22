34cb1dcdbdada5b52470a65288659420326b52c1
Ednaldo Gomes <egomes@metatron.com.br>
2026-01-22 17:30:09 -0300

    fix(global): ajusta layout do componente BigSpinner

--- src/components/BigSpinner/index.tsx
+++ src/components/BigSpinner/index.tsx
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 import loader from '../../assets/loader3.gif';
 
-const Spinner = styled.div`
+const Spinner = styled.i`
+    position: absolute;
+    top: 0;
     width: 100%;
+    height: 100vh;
+    background: url(${loader}) #ffffff59 center no-repeat;
+    background-size: 16rem;
+    backdrop-filter: blur(1px);
+    user-select: none;
+    pointer-events: all;
     min-height: 250px;
-    background: url(${loader}) center no-repeat;
-    background-size: 5rem;
 `;
 
 export const BigSpinner: React.FC = () => <Spinner/>
