22e386626fd74650c9d16fa865055ca6c6f50d4a
Ednaldo Gomes <egomes@metatron.com.br>
2026-07-28 20:19:48 -0300

    chore(push): testa imagem do jogo como thumbnail da notificação no PC

--- .github/scripts/cleanup-check.cjs
+++ .github/scripts/cleanup-check.cjs
@@ -165,12 +165,12 @@ async function sendPush(userToken, title, body, photoUrl) {
           //   body,
           //   image: photoUrl ?? "",
           // },
-          android: {
-            notification: {
-              image: photoUrl ?? "",
-              icon: photoUrl ?? "",
-            }
-          },
+          // android: {
+          //   notification: {
+          //     image: photoUrl ?? "",
+          //     icon: photoUrl ?? "",
+          //   }
+          // },
         },
       }),
       // body: JSON.stringify({
