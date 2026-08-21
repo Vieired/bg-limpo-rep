fd6978852dd2b7569a2087a851480a05f09cb6e3
Ednaldo Gomes <egomes@metatron.com.br>
2026-08-21 18:50:39 -0300

    build(test): corrige configuração para identificar caminhos de imports iniciados por "@/"

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
-// 	import { defineConfig as defineViteConfig, mergeConfig } from 'vite';
-// 	import { defineConfig as defineVitestConfig } from 'vitest/config';
-// 	import react from '@vitejs/plugin-react';
-// 	const viteConfig = defineViteConfig({
-// 	  plugins: [react()],
-// 	});
-// 	const vitestConfig = defineVitestConfig({
-// 	  test: {
-// 	    globals: true,
-// 	    environment: "jsdom",
-// 	  },
-// 	});
-// export default mergeConfig(viteConfig, vitestConfig);
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths()
+    tsconfigPaths(),
   ],
-})
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vitest/config';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
   test: {
     environment: 'jsdom',
     pool: 'forks',
-    // maxWorkers: 1,
   },
 });
